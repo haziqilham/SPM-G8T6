2c02c88410c7dd37a6b56b552a6e13d6b1681082
Jun Hong <jun_hong1997@hotmail.com>
2021-10-25 16:11:30 +0800

    Update user.py

--- user.py
+++ user.py
@@ -1,20 +1,24 @@
 class User():
 
-    __userId = None
+    __user_id = None
+    __user_name = None
     __name = None
     __designation = None
     __department = None
     __role = None
 
-    def __init__(self, id, name, designation, department, role):
-        self.__userId = id
+    def __init__(self, id, username, name, designation, department, role):
+        self.__user_id = id
+        self.__user_name = username
         self.__name = name
         self.__designation = designation
         self.__department = department
         self.__role = role
 
     def getId(self):
-        return self.__userId
+        return self.__user_id
+    def getUserName(self):
+        return self.__user_name
     def getName(self):
         return self.__name
     def getDesignation(self):
@@ -26,14 +30,27 @@ class User():
     
 
 class Administrator(User):
-    def __init__(self, id, name, designation, department, role):
-        User.__init__(self,id, name, designation, department, role)
+    def __init__(self, id, username, name, designation, department, role):
+        User.__init__(self,id, username, name, designation, department, role)
+    
+    def createCourse(self):
+        pass
+    def createClass(self):
+        pass
+    def enrollLearners(self):
+        pass
 
 
 class Learners(User):
-    def __init__(self, id, name, designation, department, role):
-        User.__init__(self,id, name, designation, department, role)
+    def __init__(self, id, username, name, designation, department, role):
+        User.__init__(self,id, username, name, designation, department, role)
+    
+    def enrollClass(self):
+        pass
 
 class Trainers(User):
-    def __init__(self, id, name, designation, department, role):
-        User.__init__(self,id, name, designation, department, role)
+    def __init__(self, id, username, name, designation, department, role):
+        User.__init__(self,id, username, name, designation, department, role)
+    
+    def createQuiz(self):
+        pass
