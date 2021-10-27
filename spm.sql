6d3555a3027832bd80852c5eacae4ee9ffe6f072
Jun Hong <jun_hong1997@hotmail.com>
2021-10-27 19:22:00 +0800

    Update spm.sql

--- spm.sql
+++ spm.sql
@@ -227,6 +227,89 @@ CREATE table `quiz`(
 INSERT INTO `quiz` (`quiz_id`, `chapter_id`, `duration`, `graded`) VALUES
 (1, 1, 20, 0);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `question_tf`
+--
+
+DROP TABLE IF EXISTS `question_tf`;
+CREATE TABLE `question_tf` (
+    `question_tf_id` INT(11) NOT NULL,
+    `corrected_value` BOOLEAN NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `question_tf`
+--
+
+INSERT INTO `question_tf` (`question_tf_id`, `corrected_value`) VALUES
+(1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `question_mcq`
+--
+
+DROP TABLE IF EXISTS `question_mcq`;
+CREATE TABLE `question_mcq` (
+    `question_mcq_id` INT(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `question_mcq`
+--
+
+INSERT INTO `question_mcq` (`question_mcq_id`) VALUES
+(2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `question`
+--
+
+DROP TABLE IF EXISTS `question`;
+CREATE table `question`(
+    `question_id` INT(11) NOT NULL,
+    `quiz_id` INT(11) NOT NULL,
+    `question` VARCHAR(250) NOT NULL,
+    `marks` INT(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table 'question'
+--
+
+INSERT INTO `question` (`question_id`, `quiz_id`, `question`, `marks`) VALUES
+(1, 1, 'Testing tf', 1),
+(2, 1, 'testing mcq', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `options`
+--
+
+DROP TABLE IF EXISTS `options`;
+CREATE TABLE `options` (
+    `options_id` INT(11) NOT NULL,
+    `question_mcq_id` INT(11) NOT NULL,
+    `value` VARCHAR(50) NOT NULL,
+    `corrected_value` BOOLEAN NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `question_mcq`
+--
+
+INSERT INTO `options` (`options_id`, `question_mcq_id`, `value`, `corrected_value`) VALUES
+(1, 2, 'A', 0),
+(2, 2, 'B', 0),
+(3, 2, 'C', 1),
+(4, 2, 'D', 0);
+
 --
 -- Indexes for dumped tables
 --
@@ -286,6 +369,37 @@ ALTER TABLE `course_progression`
   ADD PRIMARY KEY (`cc_id`);
 
 --
+-- Indexes for table `quiz`
+--
+ALTER TABLE `quiz`
+  ADD PRIMARY KEY (`quiz_id`);
+
+--
+-- Indexes for table `question_tf`
+--
+ALTER TABLE `question_tf`
+  ADD PRIMARY KEY (`question_tf_id`);
+
+--
+-- Indexes for table `question_mcq`
+--
+ALTER TABLE `question_mcq`
+  ADD PRIMARY KEY (`question_mcq_id`);
+
+--
+-- Indexes for table `question`
+--
+ALTER TABLE `question`
+  ADD PRIMARY KEY (`question_id`);
+
+--
+-- Indexes for table `options`
+--
+ALTER TABLE `options`
+  ADD PRIMARY KEY (`options_id`);
+
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -323,7 +437,20 @@ ALTER TABLE `chapter`
 -- AUTO_INCREMENT for table `course_progression`
 --
 ALTER TABLE `course_progression`
-  MODIFY `course_progression` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `cc_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `question`
+--
+ALTER TABLE `question`
+  MODIFY `question_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `options`
+--
+ALTER TABLE `options`
+  MODIFY `options_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 
 --
 -- Constraints for dumped tables
@@ -368,10 +495,40 @@ ALTER TABLE `chapter`
   ADD CONSTRAINT `chapter_fk_1` FOREIGN KEY (`class_id`) REFERENCES `class` (`class_id`);
 
 --
--- Constraints for table `class`
+-- Constraints for table `course_progression`
 --
 ALTER TABLE `course_progression`
   ADD CONSTRAINT `course_progression_fk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
   ADD CONSTRAINT `course_progression_fk_2` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_id`),
   ADD CONSTRAINT `course_progression_fk_3` FOREIGN KEY (`class_id`) REFERENCES `class` (`class_id`),
   ADD CONSTRAINT `course_progression_fk_4` FOREIGN KEY (`chapter_id`) REFERENCES `chapter` (`chapter_id`);
+
+--
+-- Constraints for table `quiz`
+--
+ALTER TABLE `quiz`
+  ADD CONSTRAINT `quiz_fk_1` FOREIGN KEY (`chapter_id`) REFERENCES `chapter` (`chapter_id`);
+
+--
+-- Constraints for table `question`
+--
+ALTER TABLE `question`
+  ADD CONSTRAINT `question_fk_1` FOREIGN KEY (`quiz_id`) REFERENCES `quiz` (`quiz_id`);
+
+--
+-- Constraints for table `question_tf`
+--
+ALTER TABLE `question_tf`
+  ADD CONSTRAINT `question_tf_fk_1` FOREIGN KEY (`question_tf_id`) REFERENCES `question` (`question_id`);
+
+--
+-- Constraints for table `question_mcq`
+--
+ALTER TABLE `question_mcq`
+  ADD CONSTRAINT `question_mcq_fk_1` FOREIGN KEY (`question_mcq_id`) REFERENCES `question` (`question_id`);
+
+--
+-- Constraints for table `options`
+--
+ALTER TABLE `options`
+  ADD CONSTRAINT `options_fk_1` FOREIGN KEY (`question_mcq_id`) REFERENCES `question` (`question_id`);
