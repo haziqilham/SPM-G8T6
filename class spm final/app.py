c51ce773ba5379f7ee46abb519cf68d6489e449d
xyyyyp <xinyi.pang.2019@sis.smu.edu.sg>
2021-11-02 23:03:58 +0800

    Update app.py

--- class spm final/app.py
+++ class spm final/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import _EngineDebuggingSignalEvents, SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import datetime as dt
 
