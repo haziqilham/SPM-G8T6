29a239ec1d3bde1564c3b69dbd5091dd2756b78d
xyyyyp <xinyi.pang.2019@sis.smu.edu.sg>
2021-11-02 23:03:11 +0800

    methods addition

--- class spm final/app.py
+++ class spm final/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import _EngineDebuggingSignalEvents, SQLAlchemy
 from flask_cors import CORS
+import datetime as dt
 
 
 app = Flask(__name__)
@@ -24,6 +25,59 @@ class User(db.Model):
     department = db.Column(db.Varchar(50), nullable=False)
     role = db.Column(db.Varchar(50), nullable=False)
 
+    def to_dict(self):
+        columns = self.__mapper__.column_attrs.keys()
+        result = {}
+        for column in columns:
+            result[column] = getattr(self, column)
+        return result
+
+    def enroll(self, classid):
+        #get Class info
+        classinfo = Class.query.filter_by(class_id = classid).first()
+
+        #get Course id from class info
+        courseid = classinfo.Course_id
+        #check Capacity
+        if Class.checkCapacity(classid):
+            #check prereq
+            prereqs = Prerequisites.query.filter_by(course_id = courseid).all()
+            #append all prereq into list
+            if len(prereqs) > 0:
+                prereqlist = []
+                for i in prereqs:
+                    prereqlist.append(i.prereq_course_id)
+
+            #check user's existing course progress
+            progress = CourseProgression.query.filter_by(user_id = self.user_id).all()
+            #check that user is not currently in progress or enrolled into the course
+            if len(progress) > 0:
+                for i in progress:
+                    if i.course_id == courseid:
+                        if i.status == "ongoing" or i.status == "enrolled":
+                            raise Exception("User is already enrolled into the course.")
+
+            #append into completed list
+            completedlist = []
+            if len(progress) > 0:
+                for i in progress:
+                    if i.status == 'completed':
+                        completedlist.append(i.course_id)
+
+            #check that user meets all prereq
+            if len(prereqlist) == 0 and len(completedlist) == 0:
+                self.to_dict(self)
+            else: 
+                if len(prereqlist) > 0 and len(completedlist) < 1:
+                    raise Exception("User does not meet the course requirements.")               
+                else:
+                    for i in prereqlist:
+                        if i not in completedlist:
+                            raise Exception("User does not meet the course requirements.")
+                        else:
+                            self.to_dict(self)
+
+
 class Course(db.Model):
     __tablename__ = 'course'
 
@@ -31,12 +85,31 @@ class Course(db.Model):
     course_name = db.Column(db.Varchar(50), nullable=False)
     archive_date = db.Column(db.Date)
 
+    def to_dict(self):
+        columns = self.__mapper__.column_attrs.keys()
+        result = {}
+        for column in columns:
+            result[column] = getattr(self, column)
+        return result    
+
+    def archivecourse(self):
+        course = Course.query.filter_by(course_id = self.course_id).first()
+        course.archive_date = dt.date.today()
+
+
 class Prerequisites(db.Model):
     __tablename__ = 'prerequisites'
 
     prereq_id = db.Column(db.Integer(11), primary_key=True, auto_increment=True)
     course_id = db.Column(db.Integer(11), db.ForeignKey('course.course_id'), nullable=False)
     prereq_course_id = db.Column(db.Integer(11), db.ForeignKey('course.course_id'), nullable=False)
+
+    def to_dict(self):
+        columns = self.__mapper__.column_attrs.keys()
+        result = {}
+        for column in columns:
+            result[column] = getattr(self, column)
+        return result
 
 class Class(db.Model):
     __tablename__ = 'class'
@@ -53,6 +126,23 @@ class Class(db.Model):
     start_enrollment = db.Column(db.Date, nullable=False)
     end_enrollment = db.Column(db.Date, nullable=False)
 
+    def to_dict(self):
+        columns = self.__mapper__.column_attrs.keys()
+        result = {}
+        for column in columns:
+            result[column] = getattr(self, column)
+        return result
+
+    #check if the class is full
+    def checkCapacity(classid):
+        enrolled = CourseProgression.query.filter_by(class_id = classid, status = "enrolled").all()
+        classinfo = Class.query.filter_by(class_id = classid).first()
+        if len(enrolled) < classinfo.capacity:
+            return True
+        else:
+            raise Exception("Class is at max capacity.")
+
+
 class Chapter(db.Model):
     __tablename__ = 'chapter'
 
@@ -61,6 +151,13 @@ class Chapter(db.Model):
     chapter_name = db.Column(db.Varchar(50), nullable=False)
     order = db.Column(db.Integer(11), nullable=False)
     chapter_materials = db.Column(db.Varchar(50), nullable=False)
+
+    def to_dict(self):
+        columns = self.__mapper__.column_attrs.keys()
+        result = {}
+        for column in columns:
+            result[column] = getattr(self, column)
+        return result
 
 class Quiz(db.Model):
     __tablename__ = 'quiz'
@@ -71,6 +168,16 @@ class Quiz(db.Model):
     graded = db.Column(db.Boolean, nullable=False)
     passing_mark = db.Column(db.Integer(11))
 
+    def to_dict(self):
+        columns = self.__mapper__.column_attrs.keys()
+        result = {}
+        for column in columns:
+            result[column] = getattr(self, column)
+        return result
+
+    def computeScore(self):
+        pass
+
 class Question(db.Model):
     __tablename__ = 'question'
 
@@ -79,16 +186,58 @@ class Question(db.Model):
     question = db.Column(db.Varchar(250), nullable=False)
     marks = db.Column(db.Integer(11), nullable=False)
 
+    def to_dict(self):
+        columns = self.__mapper__.column_attrs.keys()
+        result = {}
+        for column in columns:
+            result[column] = getattr(self, column)
+        return result
+    
+    def computeMarks(self, answer):
+        if answer == True or answer == False:
+            correctans = Questiontf.correctanswer(self.question_id)
+        else:
+            correctans = Questionmcq.correctanswer(self.question_id)
+        
+        if correctans == answer:
+            return self.marks
+        else:
+            return 0
+        
+
 class Questiontf(db.Model):
     __tablename__ = 'question_tf'
 
     question_tf_id = db.Column(db.Integer(11), db.ForeignKey('question.question_id'), primary_key=True, nullable=False)
     corrected_value = db.Column(db.Boolean, nullable=False)
 
+    def to_dict(self):
+        columns = self.__mapper__.column_attrs.keys()
+        result = {}
+        for column in columns:
+            result[column] = getattr(self, column)
+        return result
+
+    def correctanswer(self, questionid):
+        correct = Questiontf.query.filter_by(question_tf_id = questionid).first()
+        return correct.corrected_value
+
 class Questionmcq(db.Model):
     __tablename__ = 'question_mcq'
 
     question_mcq_id = db.Column(db.Integer(11), db.ForeignKey('question.question_id'), primary_key=True, nullable=False)
+
+    def to_dict(self):
+        columns = self.__mapper__.column_attrs.keys()
+        result = {}
+        for column in columns:
+            result[column] = getattr(self, column)
+        return result
+
+    #check if answer is correct
+    def correctanswer(self, questionid):
+        correct = Options.query.filter_by(question_mcq_id = questionid, correct_value = True).first()
+        return correct.value    
 
 class Options(db.Model):
     __tablename__ = 'options'
@@ -97,6 +246,13 @@ class Options(db.Model):
     question_mcq_id = db.Column(db.Integer(11), db.ForeignKey('question.question_id'), nullable=False)
     value = db.Column(db.Varchar(50), nullable=False)
     corrected_value = db.Column(db.Boolean, nullable=False)
+
+    def to_dict(self):
+        columns = self.__mapper__.column_attrs.keys()
+        result = {}
+        for column in columns:
+            result[column] = getattr(self, column)
+        return result
 
 class CourseProgression(db.Model):
     __tablename__ = 'course_progression'
@@ -109,5 +265,12 @@ class CourseProgression(db.Model):
     status = db.Column(db.Varchar(50), nullable=False)
     completion_date = db.Column(db.Date)
     score = db.Column(db.Integer)
+
+    def to_dict(self):
+        columns = self.__mapper__.column_attrs.keys()
+        result = {}
+        for column in columns:
+            result[column] = getattr(self, column)
+        return result
     
 
